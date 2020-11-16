668a2833b9987c8e81f8920b31eaa65e26175c8b
Starlight Romero <starlightromero@gmail.com>
2020-11-16 15:31:33 -0800

    removed return from function

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './Title.css'
 
-const Title = () => {
-  return (
-    <div className='Title'>
-      <h1>SFPOPOS</h1>
-      <div className='Title-Subtitle'>
-        <h3>San Francisco Privately Owned Public Open Spaces</h3>
-      </div>
+const Title = () => (
+  <div className='Title'>
+    <h1>SFPOPOS</h1>
+    <div className='Title-Subtitle'>
+      <h3>San Francisco Privately Owned Public Open Spaces</h3>
     </div>
-  )
-}
+  </div>
+)
 
 export default Title
