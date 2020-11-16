0702a64846f490feca306514ddfd80b7372dc296
Starlight Romero <starlightromero@gmail.com>
2020-11-16 15:01:30 -0800

    title component built

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Title.css'
+
+function Title () {
+  return (
+    <div className='Title'>
+      <h1>SFPOPOS</h1>
+      <div className='Title-Subtitle'>
+        <h3>San Francisco Privately Owned Public Open Spaces</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Title
