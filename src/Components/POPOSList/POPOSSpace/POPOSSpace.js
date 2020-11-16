fc73e91285e7816658198d2fd29f416bcf912634
Starlight Romero <starlightromero@gmail.com>
2020-11-16 15:33:01 -0800

    imported styling

--- src/Components/POPOSList/POPOSSpace/POPOSSpace.js
+++ src/Components/POPOSList/POPOSSpace/POPOSSpace.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './POPOSSpace.css'
 
 const POPOSSpace = props => {
   const { name, image, address } = props
   return (
-    <div>
+    <div className='POPOSSpace'>
       <img src={`${process.env.PUBLIC_URL}assets/${image}`} width='300' height='300' alt='Hello' />
       <h1>{ name }</h1>
       <div>{ address }</div>
