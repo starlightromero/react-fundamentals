d37ee64e690a5f01b3593ff751c36a4c7c2d35c7
Starlight Romero <starlightromero@gmail.com>
2020-11-16 15:49:21 -0800

    added date object to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Footer.css'
 
-const Footer = () => (
-  <div className='Footer'>
-    <p>Starlight Romero copyright 2019</p>
-  </div>
-)
+const Footer = () => {
+  const date = new Date()
+  return (
+    <div className='Footer'>
+      <p>Starlight Romero copyright { date.getFullYear() }</p>
+    </div>
+  )
+}
 
 export default Footer
