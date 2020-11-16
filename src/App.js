0e59670aac663e5a6f71a16ad59fedccad53e530
Starlight Romero <starlightromero@gmail.com>
2020-11-16 15:33:09 -0800

    imported Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import Title from './Components/Title/Title'
 import POPOSList from './Components/POPOSList/POPOSList'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <div className='App'>
       <Title />
       <POPOSList />
+      <Footer />
     </div>
   )
 }
