4a83d7e1435a1060f47e398b71c98091f0344e4c
Starlight Romero <starlightromero@gmail.com>
2020-11-16 15:30:37 -0800

    added Footer component

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => (
+  <div className='Footer'>
+    <p>Starlight Romero copyright 2019</p>
+  </div>
+)
+
+export default Footer
