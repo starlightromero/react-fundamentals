d6ad94c7c31ae459170682bbfa7938e33b8fc028
Starlight Romero <starlightromero@gmail.com>
2020-11-16 16:26:08 -0800

    added NavLinks

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Title.css'
 
 const Title = () => (
   <div className='Title'>
     <h1>SFPOPOS</h1>
-    <div className='Title-Subtitle'>
-      <h3>San Francisco Privately Owned Public Open Spaces</h3>
+    <h3 className='Title-Subtitle'>San Francisco Privately Owned Public Open Spaces</h3>
+    <div>
+      <NavLink to='/' className='nav-link' activeClassName='nav-link-active'>
+        Home
+      </NavLink>
+      <NavLink to='/about' className='nav-link' activeClassName='nav-link-active'>
+        About
+      </NavLink>
     </div>
   </div>
 )
